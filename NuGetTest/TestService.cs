c5cd8dde9e67bc6f2f1ac5cb2932700cc7938ce3
Lee Hewlett <lee.hewlett@wharncliffe.co.uk>
2022-04-08 16:22:54 +0100

    Initial commit

---
+++ NuGetTest/TestService.cs
@@ -0,0 +1,16 @@
+﻿
+namespace NuGetTest
+{
+    public interface ITestService
+    {
+        string ReturnString(string inputString);
+    }
+
+    public class TestService : ITestService
+    {
+        public string ReturnString(string inputString)
+        {
+            return inputString;
+        }
+    }
+}
